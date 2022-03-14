dc418ed74035dc815f06fd930f2c67de2c12e142
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-14 16:02:42 -0700

    add Student Index and Create.
    Co-authored-by: Alex Adamovic <alexadamovic@users.noreply.github.com>

--- UniversityRegistrar/Controllers/CoursesController.cs
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -1,11 +1,11 @@
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using System.Collections.Generic;
-// using System.Linq;
-// using UniversityRegistrar.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using UniversityRegistrar.Models;
 
-// namespace UniversityRegistrar.Controllers
-// {
-
-// }
+namespace UniversityRegistrar.Controllers
+{
+  
+}
---
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using UniversityRegistrar.Models;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly UniversityRegistrarContext _db;
+    
+    public StudentsController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      ViewBag.PageTitle = "Students";
+      var orderedList = _db.Students.OrderBy(student => student.StudentName);
+      return View(orderedList.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "Add Student";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Student student)
+    {
+      _db.Students.Add(student);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
+  }
+}
