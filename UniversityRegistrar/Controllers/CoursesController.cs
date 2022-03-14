591cf812ba452e7493a6271fb8e1abf78e983ea9
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-14 15:03:52 -0700

    add Initial Migration.
    Co-authored-by: Alex Adamovic <alexadamovic@users.noreply.github.com>

---
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -0,0 +1,11 @@
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc.Rendering;
+// using System.Collections.Generic;
+// using System.Linq;
+// using UniversityRegistrar.Models;
+
+// namespace UniversityRegistrar.Controllers
+// {
+
+// }
